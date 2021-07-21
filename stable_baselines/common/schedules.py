5cf243e57751aaab0ef17f5e8e1a8015cb278dbd
Pranay Shah <pranay96shah@gmail.com>
2021-07-21 16:09:42 +0100

    Add Exponential decay Schedule

--- stable_baselines/common/schedules.py
+++ stable_baselines/common/schedules.py
@@ -9,6 +9,7 @@ Each schedule has a function `value(t)` which returns the current value
 of the parameter given the timestep t of the optimization procedure.
 """
 
+import numpy as np
 
 class Schedule(object):
     def value(self, step):
@@ -106,7 +107,39 @@ class LinearSchedule(Schedule):
     def value(self, step):
         fraction = min(float(step) / self.schedule_timesteps, 1.0)
         return self.initial_p + fraction * (self.final_p - self.initial_p)
+      
+      
+def factorial(k):
+    if k < 2:
+        return 1
+    else:
+        return k * factorial(k - 1)
 
+     
+class ExponentialSchedule(Schedule):
+    """
+    Exponential decay between initial_p and final_p over schedule_timesteps.
+    After this many timesteps pass, final_p is returned.
+
+    :param schedule_timesteps: (int) Number of timesteps for which to linearly anneal initial_p to final_p
+    :param initial_p: (float) initial output value
+    :param final_p: (float) final output value
+    :param order: (integer) order of Taylor series approximation
+    """
+
+    def __init__(self, schedule_timesteps, final_p, initial_p=1.0, order=5):
+        self.schedule_timesteps = schedule_timesteps
+        self.final_p = final_p
+        self.initial_p = initial_p
+        self.order = order
+        self.gradient = np.exp(self.initial_p)
+
+    def value(self, step):
+        f_x = np.exp(self.initial_p)
+        for i in range(self.order):
+            n = i + 1
+            f_x += self.gradient * (1 / factorial(n)) * ((step / self.schedule_timesteps) * (self.final_p - self.initial_p)) ** n
+        return f_x
 
 def get_schedule_fn(value_schedule):
     """
